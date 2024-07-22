1c63585b5ee0996f0db6f6c1dff2c78de5a0923f
nlhusna <164849193+nlhusna@users.noreply.github.com>
2024-07-22 23:30:53 +0800

    Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        String variableString;
         Pengguna pengguna = new Pengguna();
         Buku buku = new Buku();
         Komik komik = new Komik();
         Cerpen cerpen = new Cerpen();
-    }
 
+        // Contoh penggunaan metode pada objek Cerpen
+        cerpen.setJudul("Cerpen Inspiratif");
+        cerpen.setPenulis("Penulis Terkenal");
+        cerpen.setJumlahHalaman(25);
+        cerpen.setHarga(50000);
+
+        cerpen.tampilkanDetailCerpen();
+        cerpen.tambahDiskon(5000);
+        cerpen.naikkanHarga();
+
+        // Menggunakan getter untuk mendapatkan nilai atribut
+        System.out.println("Judul Cerpen: " + cerpen.getJudul());
+        System.out.println("Penulis Cerpen: " + cerpen.getPenulis());
+        System.out.println("Jumlah Halaman Cerpen: " + cerpen.getJumlahHalaman());
+        System.out.println("Harga Cerpen: " + cerpen.getHarga());
+    }
 }
